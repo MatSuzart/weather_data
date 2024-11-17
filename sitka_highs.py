40b2cef39d0c302f05504d050daa95ff80828c36
MatSuzart <mathsuzart@outlook.com>
2024-11-17 20:06:40 -0300

    :tada: Commit inicial

---
+++ sitka_highs.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import csv
+
+path = Path('data/sitka_weather_2021_simple.csv')
+lines = path.read_text().splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+print(header_row)
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
